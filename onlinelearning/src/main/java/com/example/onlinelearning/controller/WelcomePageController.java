6bb142925a826996b1e277f564a8bfd37250b3b3
bhagiyalekshmijv <bhagiyalekshmijv4401@gmail.com>
2026-04-23 12:55:42 +0530

    online website

--- onlinelearning/src/main/java/com/example/onlinelearning/controller/WelcomePageController.java
+++ onlinelearning/src/main/java/com/example/onlinelearning/controller/WelcomePageController.java
@@ -1,24 +1,22 @@
-package com.example.myapp.controller;
+package com.example.onlinelearning.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 @Controller
-public class HelloWorldController {
+public class WelcomePageController {
 
     @GetMapping("/home")
-    public String home() {
-        return "redirect:/hello";
-    }
-
-    @GetMapping("/hello")
-    public String hello(Model model) {
+    public String home(Model model) {
         String message = "The beautiful thing about learning is that no one can take it away from you.";
         model.addAttribute("message", message);
-        String hello = "<h1>Hello, World!</h1>";
-        model.addAttribute("hello", hello);
+        
+        String heading = "<h1>Hello from Spring Boot!</h1>";
+        model.addAttribute("heading", heading);
+        
         boolean isLoggedIn = true;
         model.addAttribute("isLoggedIn", isLoggedIn);
-        return "hello";
+        
+        return "WelcomePage";
     }
 }
