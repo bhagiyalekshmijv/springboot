9f2bc4659b41394068b00500aca3a21cd259a436
bhagiyalekshmijv <bhagiyalekshmijv4401@gmail.com>
2026-05-11 22:01:13 +0530

    my app

---
+++ myapp/src/main/java/com/example/myapp/controller/UserAPIController.java
@@ -0,0 +1,28 @@
+package com.example.myapp.controller;
+
+import com.example.myapp.models.User;
+import com.example.myapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api")
+public class UserAPIController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return ResponseEntity.ok("User registered successfully");
+    }
+}
